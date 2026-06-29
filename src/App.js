4202cc71c1d62f7315c9f9ca5d9b532ac5745d27
Jimmy Spann <jimmyleespann@outlook.com>
2026-06-29 16:25:03 -0400

    remove unused variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 // import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import QuestionContainer from './components/QuestionCard/questionCard'
-import CountDownComponent from './components/QuestionCard/count-down/count-down'
 
 function App() {
 
